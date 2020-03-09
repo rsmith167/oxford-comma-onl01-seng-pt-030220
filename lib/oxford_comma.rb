aac63f712574dc4d00c5e03f7fe84a59c0aeca5f
Rwanda S <rwanda.ace@gmail.com>
2020-03-09 14:55:03 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,12 +6,12 @@ if array.size == 1
 elsif array.size == 2
   new_array = array
   last_value = new_array.pop #returns a string
-  new_array.join
+  new_array = new_array.join
   new_array << " and #{last_value}"
 else
   new_array = array
   last_value = new_array.pop
-  new_array.join(", ")
+  new_array = new_array.join(", ")
   new_array << "and " << last_value
 end
 
