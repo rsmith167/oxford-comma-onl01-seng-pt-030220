c57e2b0c32b66896c2155622ad67227de1d9f742
Rwanda S <rwanda.ace@gmail.com>
2020-03-09 14:46:18 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,11 +3,11 @@ def oxford_comma(array)
 
 if array.size == 1
    array.join
-else
+elsif array.size == 2
   new_array = array
   last_value = new_array.pop
   new_array.join(", ")
-  new_array << "and " + last_value
+  new_array << "and " + last_value.join
 end
 
 end
